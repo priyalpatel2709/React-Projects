25f69fd7d2167b59bd56a15186381f84f4c10617
priyalpatel2709 <piyu9552@gmail.com>
2022-07-19 12:34:50 +0530

    Done

--- js/New.js
+++ js/New.js
@@ -1,8 +1,8 @@
 // new Function--------------------------------------------------------------------------------
-let sum = new Function ('a','b','return a+b')
-// console.log(sum(1,3)); 
-let arr=['$1','$2','100']
-let str =arr.join('+').replace(/\$/g, "")
+// let sum = new Function ('a','b','return a+b')
+// console.log(sum(1,3));
+let arr=['$1.9000','$2.1','100']
+let str =arr.join('+').replace(/\$/g,"")
 let ans=eval(str.toString())
 console.log(ans);
 const items = [
