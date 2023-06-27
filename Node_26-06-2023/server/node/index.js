23566456b7e15d9737dfb1f5692cf7b9d68c9db5
Priyal Patel <v-patelpr@microsoft.com>
2023-06-27 12:49:26 +0530

    server

---
+++ Node_26-06-2023/server/node/index.js
@@ -0,0 +1,26 @@
+const http = require("http")
+const fs = require("fs")
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, { 'content-type' : 'text /html' })
+    fs.readFile('demo.html',(err,data)=>{
+        if(err){
+            res.writeHead(404)
+            res.write('not found')
+            
+        }else{
+            res.write(data)
+        }
+        res.end()
+    })
+    // res.write(" hey there")
+    
+})
+
+server.listen(3000,(err)=>{
+    if(err){
+        console.log("error",err);
+    }else{
+        console.log("3000 is on");
+    }
+})
+
