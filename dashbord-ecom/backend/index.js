612752f7f0b66d2fc33577463fb5ffd942416aa0
Priyal Patel <v-patelpr@microsoft.com>
2023-07-03 11:42:01 +0530

    react connected with mongoDB

--- dashbord-ecom/backend/index.js
+++ dashbord-ecom/backend/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/config')
 const User = require('./db/User')
-const bodyParser = require('body-parser');
 const app =express()
 app.use(express.json())
-
+app.use(cors())
 
 app.post('/register',(req,resp)=>{
     let users = new User(req.body)
     let result = users.save()
     resp.send(result)
-    // resp.end()
+    resp.end()
 })
 
 app.listen(5000) 
--- dashbord-ecom/front-end/src/component/SingUp.jsx
+++ dashbord-ecom/front-end/src/component/SingUp.jsx
@@ -1,19 +1,31 @@
-import React,{useState} from "react";
-
+import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom'
 const SingUp = () => {
-    const [name,setName]=useState('')
-    const [password,setPassword]=useState('')
-    const [email,setEmail]=useState('')
-
-    const collectData =()=>{
-        console.log(`
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate()
+  const collectData =async () => {
+    console.log(`
         name:-${name}
         password:-${password}
-        email:-${email} `);     
-    }
+        email:-${email} `);
+
+      let result = await fetch('http://127.0.0.1:5000/register',{
+            method : 'post',
+            body : JSON.stringify({name,password,email}),
+            headers : {
+              "Content-Type" : "application/json"
+            }
+      })
+      result = await result.json()
+      console.log(result);
+      if(result){
+        navigate('/')
+      }
+  };
 
   return (
-
     <div>
       <h1>Sing Up</h1>
       <div>
@@ -22,25 +34,30 @@ const SingUp = () => {
           type="text"
           placeholder="Enter Your Name"
           value={name}
-          onChange={(e)=>setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           className="input-box"
           type="text"
           placeholder="Enter Your Email"
           value={email}
-          onChange={(e)=>setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           className="input-box"
           type="password"
           placeholder="Enter Your Password"
           value={password}
-          onChange={(e)=>setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        
       </div>
-      <button style={{marginBottom: '10px'}} onClick={collectData} type="button">Sing Up</button>
+      <button
+        style={{ marginBottom: "10px" }}
+        onClick={collectData}
+        type="button"
+      >
+        Sing Up
+      </button>
     </div>
   );
 };
