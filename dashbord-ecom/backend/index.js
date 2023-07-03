32f95800ad777a045d2a3c77df7bb0ef364b35e9
Priyal Patel <v-patelpr@microsoft.com>
2023-07-03 15:03:15 +0530

    find user in DB working

--- dashbord-ecom/backend/index.js
+++ dashbord-ecom/backend/index.js
@@ -1,18 +1,34 @@
-const express = require('express')
-const cors = require('cors')
-require('./db/config')
-const User = require('./db/User')
-const app =express()
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.post('/register',(req,resp)=>{
-    let users = new User(req.body)
-    let result = users.save()
-    resp.send(result)
-    resp.end()
-})
+app.post("/register", (req, resp) => {
+  let users = new User(req.body);
+  let result = users.save();
+  resp.send(result);
+  resp.end();
+});
 
-app.listen(5000,()=>{
-    console.log("Server is running on port http://127.0.0.1:5000")
-}) 
+app.post("/login", async (req, resp) => {
+  // resp.send(req.body)
+//   console.log(req.body);
+
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "user not found" });
+    }
+  } else {
+    resp.send({ result: "user not found" });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port http://127.0.0.1:5000");
+});
