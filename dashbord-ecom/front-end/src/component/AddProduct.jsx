299c12a7d8d60d769ffb174c4fcd974af28ea8a5
Priyal Patel <v-patelpr@microsoft.com>
2023-07-04 09:37:11 +0530

    with error msg

--- dashbord-ecom/front-end/src/component/AddProduct.jsx
+++ dashbord-ecom/front-end/src/component/AddProduct.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "../styles/AddProduct.css";
+
 
 const AddProduct = () => {
   const [values, setValues] = useState({
@@ -6,6 +8,7 @@ const AddProduct = () => {
     price: "",
     category: "",
     company: "",
+    error: false,
   });
 
   const handelChange = (e) => {
@@ -18,28 +21,41 @@ const AddProduct = () => {
     });
   };
 
-  
-
-  const collectData = async () =>{
-    console.log(values.name);
-    let result = await fetch(`http://127.0.0.1:5000/add-product`,{
-        method :"post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            name: values.name,
-            price: values.price,
-            category: values.category,
-            company: values.company,
-          }),
-    })
-    result = await result.json()
-    console.log(result);
-    if(result){
-      alert("Added Successfully")
-      setValues({})
+  const collectData = async () => {
+    if (!values.name || !values.category || !values.company || !values.price) {
+     
+      setValues((preVal) => ({
+        ...preVal,
+        error: true,
+      }));
+      return false;
     }
-    
-  }
+
+    const userId = JSON.parse(localStorage.getItem("user"))._id;
+    let result = await fetch(`http://127.0.0.1:5000/add-product`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: values.name,
+        price: values.price,
+        category: values.category,
+        company: values.company,
+        userId: userId,
+      }),
+    });
+    result = await result.json();
+    console.log(result);
+    if (result) {
+      localStorage.setItem("product", JSON.stringify(result));
+      alert("Added Successfully");
+      setValues({
+        name: "",
+        price: "",
+        category: "",
+        company: "",
+      });
+    }
+  };
 
   return (
     <div>
@@ -52,28 +68,36 @@ const AddProduct = () => {
             placeholder="Enter Name of Product"
             name="name"
             onChange={handelChange}
+            value={values.name}
           />
+          {values.error && !values.name && <span className="err-span">enter name*</span>}
           <input
             className="input-box"
             type="text"
             placeholder="Enter the Price of Product"
             name="price"
             onChange={handelChange}
+            value={values.price}
           />
+          {values.error && !values.price && <span className="err-span">enter price*</span>}
           <input
             className="input-box"
             type="text"
             placeholder="Enter the Category of Product"
             name="category"
             onChange={handelChange}
+            value={values.category}
           />
+          {values.error && !values.category && <span className="err-span">enter category*</span>}
           <input
             className="input-box"
             type="text"
             placeholder="Enter the Company of Product"
             name="company"
             onChange={handelChange}
+            value={values.company}
           />
+          {values.error && !values.company && <span className="err-span">enter company*</span>}
         </div>
         <button
           // style={{ marginBottom: "10px" }}
@@ -82,7 +106,6 @@ const AddProduct = () => {
         >
           Add Product
         </button>
-
       </div>
     </div>
   );
