3f7b85ee592317d8d8f899710f5bc56aff5bc455
priyalpatel2709 <piyu9552@gmail.com>
2023-07-07 14:53:28 +0530

    with live srever mongo

--- dashbord-ecom/front-end/src/component/Admin.jsx
+++ dashbord-ecom/front-end/src/component/Admin.jsx
@@ -6,6 +6,8 @@ const Admin = () => {
   const [data, setData] = useState({
     users: [],
     products: [],
+    userProcuct: [],
+    userName: "",
   });
 
   useEffect(() => {
@@ -22,7 +24,7 @@ const Admin = () => {
 
         setData((prevData) => ({
           ...prevData,
-          users: [ ...arrData[0]],
+          users: [...arrData[0]],
         }));
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -32,11 +34,23 @@ const Admin = () => {
     getData();
   }, []);
 
+  const productOfuser = (userId, name) => {
+    const userProducts = data.products.filter(
+      (product) => product.userId === userId
+    );
+    setData((preval) => ({
+      ...preval,
+      userProcuct: [...userProducts],
+      userName: name,
+    }));
+  };
+
   const mapprduser = data.users
     .filter((val) => val.name && val.email && val.password)
-    .map((val) => (
-      <tr key={val._id}>
-        <td>{val.name}</td>
+    .map((val, i) => (
+      <tr key={val._id} onClick={() => productOfuser(val._id, val.name)}>
+        <td>{i+1}</td>
+        <td >{val.name}</td>
         <td>{val.email}</td>
         <td>{val.password}</td>
       </tr>
@@ -44,11 +58,12 @@ const Admin = () => {
 
   const maapedProduct = data?.products
     .filter(
-      (val,i) =>
+      (val, i) =>
         val.category && val.company && val.name && val.price && val.userId
     )
-    .map((val,i) => (
-      <tr key={i}>
+    .map((val, i) => (
+      <tr key={i + 1}>
+        <td>{i+1}</td>
         <td>{val.category}</td>
         <td>{val.company}</td>
         <td>{val.name}</td>
@@ -57,33 +72,77 @@ const Admin = () => {
       </tr>
     ));
 
-    console.log("maapedProduct",maapedProduct);
-  return (
-    <div className="table-container">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Password</th>
-          </tr>
-        </thead>
-        <tbody>{mapprduser}</tbody>
-      </table>
+  const maapeduserProcuct = data?.userProcuct
+    .filter(
+      (val) =>
+        val.category && val.company && val.name && val.price && val.userId
+    )
+    .map((val, i) => (
+      <tr key={i}>
+        <td>{i + 1}</td>
+        <td>{val.category}</td>
+        <td>{val.company}</td>
+        <td>{val.name}</td>
+        <td>{val.price}</td>
+      </tr>
+    ));
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>Company</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>User ID</th>
-          </tr>
-        </thead>
-        <tbody>{maapedProduct}</tbody>
-      </table>
-    </div>
+  return (
+    <>
+      <div className="table-container">
+        <h3>Usre Info</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Sr.No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+            </tr>
+          </thead>
+          <tbody>{mapprduser}</tbody>
+        </table>
+        <h3>Product Info</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Sr.no</th>
+              <th>Category</th>
+              <th>Company</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>User ID</th>
+            </tr>
+          </thead>
+          <tbody>{maapedProduct}</tbody>
+        </table>
+      </div>
+
+      {data.userProcuct.length > 0 ? (
+        <>
+        <h2 className="title">{`${data.userName} user Info`}</h2>
+           
+          <table className="my-table">
+            <thead>
+              <tr>
+                <th>Sr.No</th>
+                <th>Category</th>
+                <th>Company</th>
+                <th>Name</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>{maapeduserProcuct}</tbody>
+          </table>
+        </>
+      ) : (
+        data.userName && (
+          <p style={{ paddingLeft: "43%", paddingTop: "7%", color: "red" }}>
+            {data.userName} has not added any products.
+          </p>
+        )
+      )}
+    </>
   );
 };
 
