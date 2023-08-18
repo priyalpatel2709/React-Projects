3bfe09129623c55b3a653d7f4c8af6da18979322
priyalpatel2709 <piyu9552@gmail.com>
2023-08-18 16:14:35 +0530

    need to work

--- redux-toolkit/src/components/NavBar.js
+++ redux-toolkit/src/components/NavBar.js
@@ -1,36 +1,42 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 
 const NavBarCom = () => {
   return (
     <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-    <Container fluid>
-      <Navbar.Brand href="#">Reduc Toolkit</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav >
-          <Nav.Link to="/" as={Link}>Home</Nav.Link>
-        </Nav>
-        <Navbar.Collapse className="d-flex">
-          <Navbar.Text>
-          <Nav.Link to="/addcart" as={Link}>My Bag 0</Nav.Link>
-          </Navbar.Text>
-        </Navbar.Collapse>
-        
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  <Outlet/>
-  </>
+      <Provider store={store}>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand href="#">Reduc Toolkit</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav>
+                <Nav.Link to="/" as={Link}>
+                  Home
+                </Nav.Link>
+              </Nav>
+              <Navbar.Collapse className="d-flex">
+                <Navbar.Text>
+                  <Nav.Link to="/addcart" as={Link}>
+                    My Bag 0
+                  </Nav.Link>
+                </Navbar.Text>
+              </Navbar.Collapse>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Outlet />
+      </Provider>
+    </>
   );
 };
 
