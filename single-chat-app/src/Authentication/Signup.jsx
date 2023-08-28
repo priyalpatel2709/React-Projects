b5bdfdb032962cb24167c4804619469d1561b5a9
priyalpatel2709 <piyu9552@gmail.com>
2023-08-28 10:30:44 +0530

    pic upload working

--- single-chat-app/src/Authentication/Signup.jsx
+++ single-chat-app/src/Authentication/Signup.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
   const toast = useToast();
-//   const history = useHistory();
+  //   const history = useHistory();
 
   const [name, setName] = useState();
   const [email, setEmail] = useState();
@@ -20,73 +20,73 @@ const Signup = () => {
   const [pic, setPic] = useState();
   const [picLoading, setPicLoading] = useState(false);
 
-//   const submitHandler = async () => {
-//     setPicLoading(true);
-//     if (!name || !email || !password || !confirmpassword) {
-//       toast({
-//         title: "Please Fill all the Feilds",
-//         status: "warning",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "bottom",
-//       });
-//       setPicLoading(false);
-//       return;
-//     }
-//     if (password !== confirmpassword) {
-//       toast({
-//         title: "Passwords Do Not Match",
-//         status: "warning",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "bottom",
-//       });
-//       return;
-//     }
-//     console.log(name, email, password, pic);
-//     try {
-//       const config = {
-//         headers: {
-//           "Content-type": "application/json",
-//         },
-//       };
-//       const { data } = await axios.post(
-//         "/api/user",
-//         {
-//           name,
-//           email,
-//           password,
-//           pic,
-//         },
-//         config
-//       );
-//       console.log(data);
-//       toast({
-//         title: "Registration Successful",
-//         status: "success",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "bottom",
-//       });
-//       localStorage.setItem("userInfo", JSON.stringify(data));
-//       setPicLoading(false);
-//       history.push("/chats");
-//     } catch (error) {
-//       toast({
-//         title: "Error Occured!",
-//         description: error.response.data.message,
-//         status: "error",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "bottom",
-//       });
-//       setPicLoading(false);
-//     }
-//   };
+  //   const submitHandler = async () => {
+  //     setPicLoading(true);
+  //     if (!name || !email || !password || !confirmpassword) {
+  //       toast({
+  //         title: "Please Fill all the Feilds",
+  //         status: "warning",
+  //         duration: 5000,
+  //         isClosable: true,
+  //         position: "bottom",
+  //       });
+  //       setPicLoading(false);
+  //       return;
+  //     }
+  //     if (password !== confirmpassword) {
+  //       toast({
+  //         title: "Passwords Do Not Match",
+  //         status: "warning",
+  //         duration: 5000,
+  //         isClosable: true,
+  //         position: "bottom",
+  //       });
+  //       return;
+  //     }
+  //     console.log(name, email, password, pic);
+  //     try {
+  //       const config = {
+  //         headers: {
+  //           "Content-type": "application/json",
+  //         },
+  //       };
+  //       const { data } = await axios.post(
+  //         "/api/user",
+  //         {
+  //           name,
+  //           email,
+  //           password,
+  //           pic,
+  //         },
+  //         config
+  //       );
+  //       console.log(data);
+  //       toast({
+  //         title: "Registration Successful",
+  //         status: "success",
+  //         duration: 5000,
+  //         isClosable: true,
+  //         position: "bottom",
+  //       });
+  //       localStorage.setItem("userInfo", JSON.stringify(data));
+  //       setPicLoading(false);
+  //       history.push("/chats");
+  //     } catch (error) {
+  //       toast({
+  //         title: "Error Occured!",
+  //         description: error.response.data.message,
+  //         status: "error",
+  //         duration: 5000,
+  //         isClosable: true,
+  //         position: "bottom",
+  //       });
+  //       setPicLoading(false);
+  //     }
+  //   };
 
-const postDetails = async (pics) => {
+  const postDetails = async (pics) => {
     setPicLoading(true);
-    
+
     if (pics === undefined) {
       toast({
         title: "Please Select an Image!",
@@ -98,22 +98,22 @@ const postDetails = async (pics) => {
       setPicLoading(false);
       return;
     }
-  
+
     try {
       if (pics.type === "image/jpeg" || pics.type === "image/png") {
         const data = new FormData();
         data.append("file", pics);
         data.append("upload_preset", "Chat-app-user");
         data.append("cloud_name", "dtzrtlyuu");
-        
+
         const response = await fetch(
-          "https://res.cloudinary.com/dtzrtlyuu/image/upload",
+          "https://api.cloudinary.com/v1_1/dtzrtlyuu/image/upload",
           {
             method: "post",
             body: data,
           }
         );
-  
+
         const responseData = await response.json();
         setPic(responseData.url.toString());
         console.log(responseData.url.toString());
@@ -141,7 +141,6 @@ const postDetails = async (pics) => {
       setPicLoading(false);
     }
   };
-  
 
   return (
     <VStack spacing="5px">
